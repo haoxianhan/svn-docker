e10166d58040728461d34f1029ccbc839dafb366
haoxian <haoxianhan@gmail.com>
2022-07-04 20:07:18 +0800

    complete the example

--- example/start.sh
+++ example/start.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
-docker-compose down
+source ".env"
 
-if [ ! -d "${PWD}/svn/" ]; then
-    echo "todo"
-    echo "first start, initial..."
-fi
+try_create_volume()
+{
+  volume_name=$1
+  result=$(docker volume ls -q | grep -c ^"$volume_name"$)
 
-docker-compose up -d
+  if [ "$result" -gt 0 ]; then
+    echo "creating volume: $volume_name"
+    docker volume create "$volume_name"
+  fi
+}
 
+create_volumes()
+{
+  try_create_volume "${CONTAINER_NAME}"-svn_repo
+  try_create_volume "${CONTAINER_NAME}"-svn_config
+  try_create_volume "${CONTAINER_NAME}"-svnadmin_config
+}
+
+
+main()
+{
+  docker-compose down
+
+  create_volumes
+
+  docker-compose up -d
+}
+
+main
 
 # first setup
 # docker exec -t <container_name> htpasswd -b /etc/subversion/passwd <username> <password>
